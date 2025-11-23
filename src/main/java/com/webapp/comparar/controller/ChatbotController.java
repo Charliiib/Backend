737b234b3555c06ac8a9faaedb751bab07210d12
Charli <barros.rcarlos@gmail.com>
2025-11-23 20:11:59 -0300

    new timmer

--- src/main/java/com/webapp/comparar/controller/ChatbotController.java
+++ src/main/java/com/webapp/comparar/controller/ChatbotController.java
@@ -48,7 +48,7 @@ public class ChatbotController {
         Runnable keepAlive = () -> {
             try {
                 while (true) {
-                    Thread.sleep(1000);
+                    Thread.sleep(9000);
 
                     emitter.send(SseEmitter.event()
                             .name("keepalive")
