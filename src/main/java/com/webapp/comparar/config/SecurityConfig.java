4f21bbf656144ff0092b4892f2ffdc87533a5fd5
Charli <barros.rcarlos@gmail.com>
2025-11-23 19:08:55 -0300

    back

--- src/main/java/com/webapp/comparar/config/SecurityConfig.java
+++ src/main/java/com/webapp/comparar/config/SecurityConfig.java
@@ -62,14 +62,7 @@ public class SecurityConfig {
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
-                // Opcional: excluir patrones específicos del security chain
-                .securityMatcher("/api/**")
-                .authorizeHttpRequests(auth -> auth
-                                .requestMatchers("/api/chatbot/**").permitAll()
-                        // ... otros matchers
-                );
-
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
